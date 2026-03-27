33224060adccdfa53468739fb34b06e8d5c1d669
ZHANGJINCHENG <darkrio@qq.com>
2026-03-27 10:51:50 +0800

    init xcode project

---
+++ AutoLedger/AutoLedger/App/AutoLedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoLedgerApp.swift
+//  AutoLedger
+//
+//  Created by 张津铖 on 2026/3/27.
+//
+
+import SwiftUI
+
+@main
+struct AutoLedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
